76fdd58600b8b3f0f7942c4536962f93ea61ee46
thetopsin <thetopsyn@gmail.com>
2025-05-14 16:24:14 -0400

    agrego promedio y total de ingresos

--- taller1.c
+++ taller1.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 
 int main(){
-    int suma, ingresos, s,num;
+    double promedio, suma;
+    int ingresos, s, num;
     s = 0;
+    ingresos = 0;
     while (s != 1){
         printf("Ingrese un valor del 1 al 7. ingrese 0 para finalizar__");
         scanf("%d", &num);
         if (num == 0){
             s = s + 1;
-            printf("\nIngreso de datos finalizado");
+            printf("\n------Ingreso de datos finalizado------\n");
         }
         else{
             if (num>0 && num<8){
             printf("\nEl numero que ingreso es: %d\n\n",num);
+            suma = suma + num;
+            ingresos = ingresos + 1;
         }else{
-            printf("Error al ingresar el dato\n");
+            printf("XXXXX ERROR AL INGRESAR (DATO NO VALIDO) XXXXX\n");
         }
         }
-
-
     }
+    promedio = suma / ingresos;
+    printf("El promedio es: %.2f\nIngresaste %d notas validas\n", promedio, ingresos);
 
 
     return 0;
