9be1cd439450f50b439a53d862dd9ca12c9a6f48
thetopsin <thetopsyn@gmail.com>
2025-05-14 00:43:05 -0400

    code1

--- taller1.c
+++ taller1.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
 int main(){
-    
+    int suma, ingresos, s,num;
+    s = 0;
+    while (s != 1){
+        printf("Ingrese un valor del 1 al 7. ingrese 0 para finalizar\n");
+        scanf("%d", num);
+        if (num == 0){
+            s = s + 1;
+            printf("\nIngreso de datos finalizado");
+        }
+        else{
+            if (num>0 && num<8){
+            printf("El numero que ingreso es: %d",num);
+        }
+        }
+
+
+    }
 
 
     return 0;
