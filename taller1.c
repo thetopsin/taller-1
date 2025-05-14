62e72c5c61221ba9a47fe19e17dcb47e717fe1cb
thetopsin <thetopsyn@gmail.com>
2025-05-14 01:37:08 -0400

    vamo bien chaval

--- taller1.c
+++ taller1.c
@@ -5,14 +5,14 @@ int main(){
     s = 0;
     while (s != 1){
         printf("Ingrese un valor del 1 al 7. ingrese 0 para finalizar\n");
-        scanf("%d", num);
+        scanf("%d", &num);
         if (num == 0){
             s = s + 1;
             printf("\nIngreso de datos finalizado");
         }
         else{
             if (num>0 && num<8){
-            printf("El numero que ingreso es: %d",num);
+            printf("El numero que ingreso es: %d\n",num);
         }
         }
 
