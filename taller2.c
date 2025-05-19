1b6ec1024a40f1960657586415ad5bc2a3548c62
thetopsin <thetopsyn@gmail.com>
2025-05-19 16:22:59 -0400

    agrego primer taller2.c

---
+++ taller2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int hora, minuto, dia;
+    printf("¿Que dia se estaciono? \n[1] Para los dias desde el lunes al sabado\n[2] Para los domingos\nOpcion: ");
+    scanf("%d", &dia);
+    if (dia == 1){
+        
+
+    }
+
+
+
+    return 0;
+}
